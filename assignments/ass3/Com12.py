f8b4af7a1774b5286e5942399a6fc31c3925cfc0
Ciaran Huang-Ryan <6530166-Ciaran.HR@users.noreply.gitlab.com>
2025-05-11 15:58:31 +1000

    com12 done

---
+++ assignments/ass3/Com12.py
@@ -0,0 +1,44 @@
+
+def reprod(x):
+    """pig reproduction"""
+    if x <= 0:
+        return 0
+    else:
+        return round(x + x*(420-x)/5000)
+    
+def damage(t):
+    """pig damage on trees"""
+    d = 2
+    dt = min(abs(t-2), abs((52+t)-2), abs(t-(52+2)))
+    if dt < 10:
+        d += 8*(1-(dt/10)**2)**2
+    dt = min(abs(t-17), abs((52+t)-17), abs(t-(52+17)))
+    if dt < 7:
+        d += 6*(1-(dt/7)**2)**2
+    return d   
+
+def trap(x):
+    """pigs murdered for a single trap"""
+    return round(.03*x)
+
+
+_pig_plan = {}
+"""
+t weeks
+s 
+
+"""
+
+def pig_management(t, s, y):
+    if t == 52:
+        return (50*s, "end")
+    elif (t, s, y) not in _pig_plan:
+        _pig_plan[t, s, y] =  min((damage(t)*s + \
+                        pig_management(t+1, reprod(s)-a*trap(s), y-a)[0], a)
+                        for a in range(min(5, y+1)))
+        
+    return _pig_plan[t, s, y]
+
+print(pig_management(0, 82, 27))
+
+
