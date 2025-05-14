75ef16ab28969b98c300c0ab5018cc44a0efc5fb
Ciaran Huang-Ryan <6530166-Ciaran.HR@users.noreply.gitlab.com>
2025-05-14 14:55:42 +1000

    trapping printing

--- assignments/ass3/Com11.py
+++ assignments/ass3/Com11.py
@@ -34,11 +34,30 @@ def pig_management(t, s, y):
         return (0, "end")
     elif (t, s, y) not in _pig_plan:
         _pig_plan[t, s, y] =  min((damage(t)*s + \
-                        pig_management(t+1, reprod(s)-a*trap(s), y-a)[0], a)
+                        pig_management(t+1, reprod(s)-a*trap(s), y-a)[0], a, reprod(s)-a*trap(s))
                         for a in range(min(5, y+1)))
         
     return _pig_plan[t, s, y]
 
+def show_strategy():
+    strategy = {}
+    max = 51
+    s = 82
+    a = 27
+    for t in range(max+1):
+        strategy[t] = pig_management(t, s, a)[1]
+        s = pig_management(t, s, a)[2] 
+        a = a - strategy[t] 
+        t += 1
+    return strategy
+        
+
+     
+
 print(pig_management(0, 82, 27))
+trap_info = show_strategy()
+trap_weeks = {key: value for key, value in trap_info.items() if value != 0}
+print(trap_weeks)
+
 
 
